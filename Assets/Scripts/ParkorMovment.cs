e40c84b3102dcca8e193276d4e6097566d239184
Sael22 <165108530+Sael22@users.noreply.github.com>
2024-12-17 18:14:57 +0300

    MOvment

---
+++ Assets/Scripts/ParkorMovment.cs
@@ -0,0 +1,99 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParkorMovment : MonoBehaviour
+{
+    // Movement variables
+    public float moveSpeed = 10f;
+    public float jumpForce = 7f;
+    public float gravity = -9.81f;
+    public float climbSpeed = 5f;
+
+    // Ground and climbing checks
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+
+    public Transform climbCheck;
+    public float climbDistance = 0.5f;
+    public LayerMask climbableMask;
+
+    private CharacterController controller;
+    private Vector3 velocity;
+    private bool isGrounded;
+    private bool isClimbing;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleClimbing();
+    }
+
+    void HandleMovement()
+    {
+        // Check if grounded
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        // Get input
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        controller.Move(move * moveSpeed * Time.deltaTime);
+
+        // Jumping
+        if (Input.GetButtonDown("Jump") && isGrounded && !isClimbing)
+        {
+            velocity.y = Mathf.Sqrt(jumpForce * -2f * gravity);
+        }
+
+        // Apply gravity
+        if (!isClimbing)
+        {
+            velocity.y += gravity * Time.deltaTime;
+            controller.Move(velocity * Time.deltaTime);
+        }
+    }
+
+    void HandleClimbing()
+    {
+        // Check for climbable objects
+        isClimbing = Physics.CheckSphere(climbCheck.position, climbDistance, climbableMask);
+
+        if (isClimbing)
+        {
+            // Disable gravity and move upwards
+            velocity.y = 0;
+            float verticalInput = Input.GetAxis("Vertical");
+            Vector3 climbDirection = transform.up * verticalInput;
+            controller.Move(climbDirection * climbSpeed * Time.deltaTime);
+        }
+    }
+
+    void OnDrawGizmos()
+    {
+        // Visualize ground and climb checks
+        if (groundCheck != null)
+        {
+            Gizmos.color = Color.green;
+            Gizmos.DrawWireSphere(groundCheck.position, groundDistance);
+        }
+
+        if (climbCheck != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawWireSphere(climbCheck.position, climbDistance);
+        }
+    }
+}
